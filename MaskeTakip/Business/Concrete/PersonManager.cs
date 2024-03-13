411c0da5eb09e87d21ef6959678a0943aac02dec
Selim Hasenekoğlu <101149941+hasenekoglu@users.noreply.github.com>
2024-03-13 17:28:44 +0300

    1.Gun/1.Odev

---
+++ MaskeTakip/Business/Concrete/PersonManager.cs
@@ -0,0 +1,36 @@
+﻿using Business.Abstract;
+using Entities.Concrete;
+using MernisServiceReference;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class PersonManager : IApplicantService
+    {
+        //encapsulation
+        public void ApplyFormask(Person person)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Person> GetList() 
+        {
+            return null;
+        }
+
+        public bool CheckPerson(Person person)
+        {
+        KPSPublicSoapClient client = new KPSPublicSoapClient(KPSPublicSoapClient.EndpointConfiguration.KPSPublicSoap);
+            return client.TCKimlikNoDogrulaAsync
+                (new TCKimlikNoDogrulaRequest
+                (new TCKimlikNoDogrulaRequestBody(person.NationalIdentitiy,person.FirstName,person.LastName,person.DateOfBirthYear)))
+                .Result.Body.TCKimlikNoDogrulaResult;
+        }
+
+    
+    }
+}
---
+++ MaskeTakip/Workaround/Program.cs
@@ -0,0 +1,25 @@
+﻿using Business.Concrete;
+using Entities.Concrete;
+
+namespace Workaround
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+           
+
+            Person person1 = new Person();  
+            Person person2 = new Person();  
+
+            person1.FirstName = "Alper";
+            person1.LastName ="Onat";
+            person1.NationalIdentitiy =123;
+            person1.DateOfBirthYear = 2001;
+            person2.FirstName = "Selim";
+
+            PttManager pttManager = new PttManager(new PersonManager());
+            pttManager.GiveMask(person1);
+        }
+    }
+}
